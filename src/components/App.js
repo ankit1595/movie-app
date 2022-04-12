82b5a086e7188f59c9968737f245dc416f611c49
Ankit <ankit.95sharma@gmail.com>
2022-04-13 03:17:44 +0530

    Added UI

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,26 @@
+import { data } from "../data";
+import Navbar from "./Navbar";
+import MovieCard from "./MovieCard";
+
 function App() {
-  return <div>Project setup</div>;
+  return (
+    <div>
+      <Navbar />
+      <div className="main">
+        <div className="tabs">
+          <div className="tab">Movies</div>
+          <div className="tab">Favorites</div>
+        </div>
+        <div className="list">
+          {data.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+
+        {/* <MovieCard /> */}
+      </div>
+    </div>
+  );
 }
 
 export default App;
